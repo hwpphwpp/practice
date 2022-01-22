3ccf12d8714114b5324155fdada0981c2793449f
hwpphwpp <huiwonpark78@gmail.com>
2022-01-23 00:58:52 +0900

    react practice 6_effects

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -2,11 +2,30 @@
 import Button from "./Button";
 import styles from "./App.module.css";
 
+import {useState, useEffect} from "react";
+
 function App() {
+  const [counter,setValue] = useState(0); 
+  const [keyword, setKeyword]=useState("");
+  const onClick=()=>setValue((prev)=>prev+1);
+  const onChange=(event)=>setKeyword(event.target.value);
+  console.log("i run all the time");
+    
+  useEffect(()=>{
+    console.log("CALL THE API...");
+  },[]);
+
+  useEffect(()=>{
+   if(keyword !=="" && keyword.length > 5){
+    console.log("search for", keyword);
+   }
+  }, [keyword]);
+
   return (
     <div>
-      <h1 className={styles.title}>welcome back!!!</h1>
-      <Button banana={"Continue"} />
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here.."></input>
+      <h1>{counter}</h1>
+      <button onClick={onClick} >Click me!</button>
     </div>
   );
 }
