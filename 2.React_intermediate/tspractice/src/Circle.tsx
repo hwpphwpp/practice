f33a0b84593aa46b33653b5b98d25c96d1e5597a
hwpphwpp <huiwonpark78@gmail.com>
2022-01-27 23:26:41 +0900

    react_2.typescript practice_2

--- 2.React_intermediate/tspractice/src/Circle.tsx
+++ 2.React_intermediate/tspractice/src/Circle.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 interface ContainerPops{
     bgColor:string;
+    borderColor:string;
 }
 
 const Container=styled.div<ContainerPops>`
@@ -9,13 +10,19 @@ const Container=styled.div<ContainerPops>`
     height:200px;
     background-color:${(props)=>props.bgColor};
     border-radius:100px;
+    border:2px solid ${props=>props.borderColor};
 `;
 
 interface CircleProps{
     bgColor:string;
+    borderColor?: string;
+    text?:string;
 }
-function Circle({bgColor}:CircleProps){
-    return <Container bgColor={bgColor}/>;
+
+function Circle({bgColor, borderColor, text="default text"}:CircleProps){
+    return <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+        {text}
+        </Container>;
 }
 
 export default Circle;
