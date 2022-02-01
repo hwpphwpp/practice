a4419bff7f62d145cf5e334e0708a934f5d7b800
hwpphwpp <huiwonpark78@gmail.com>
2022-02-01 20:25:57 +0900

    2.React2_coinTracker_10

--- 2.React_intermediate/tspractice/src/routes/Chart.tsx
+++ 2.React_intermediate/tspractice/src/routes/Chart.tsx
@@ -1,13 +1,53 @@
 import { fetchCoinHistory } from "../api";
 import { useQuery } from "react-query";
+import ApexChart from "react-apexcharts";
+
+interface IHistorical {
+    time_open: string;
+    time_close: string;
+    open: number;
+    high: number;
+    low: number;
+    close: number;
+    volume: number;
+    market_cap: number;
+  }
 
 interface ChartProps{
     coinId:string; 
 }
 
 function Chart({coinId}:ChartProps){
-    const {isLoading, data} = useQuery(["ohlcv",coinId], ()=> fetchCoinHistory(coinId));
-    return <h1>Chart</h1>
+    const {isLoading, data} = useQuery<IHistorical[]>(["ohlcv",coinId], ()=> fetchCoinHistory(coinId));
+    return <div>{isLoading?"loading chart..":
+    <ApexChart 
+    type="line" 
+    series={[
+        {
+            name:"price",
+            data:data?.map(price=>price.close) 
+        },
+    ]} //보낼 data
+    options={{
+        theme:{
+            mode:"dark",
+        },
+        chart:{
+            height:300,
+            width:500,
+            toolbar:{
+                show:false,
+            },
+            background:"transparent",
+        },
+        grid:{show:false},
+        stroke:{
+            curve:"smooth",
+            width:3,
+        },
+        yaxis:{show:false,},
+        xaxis:{labels:{show:false},},
+    }}/>}</div>
 }
 
 export default Chart;
