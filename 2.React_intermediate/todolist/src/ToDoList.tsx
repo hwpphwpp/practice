d0ed3298d16c411052d59ad7253286152d7fe25d
hwpphwpp <huiwonpark78@gmail.com>
2022-02-03 21:18:57 +0900

    2.React2_todo_3

--- 2.React_intermediate/todolist/src/ToDoList.tsx
+++ 2.React_intermediate/todolist/src/ToDoList.tsx
@@ -1,34 +1,6 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-
-/* function ToDoList() {
-  const [toDo, setToDo] = useState("");
-  const [toDoError, setToDoError] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setToDoError("");
-    setToDo(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    if (toDo.length < 10) {
-      return setToDoError("To do should be longer");
-    }
-    console.log("submit");
-  };
-  return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={toDo} placeholder="Write a to do" />
-        <button>Add</button>
-        {toDoError !== "" ? toDoError : null}
-      </form>
-    </div>
-  );
-} */
-
+ 
 interface IForm {
   email: string;
   firstName: string;
@@ -36,6 +8,7 @@ interface IForm {
   username: string;
   password: string;
   password1: string;
+  extraError?:string; //전체폼에 해당되는 에러
 }
 
 function ToDoList() {
@@ -43,13 +16,19 @@ function ToDoList() {
     register,
     handleSubmit,
     formState: { errors },
+    setError
   } = useForm<IForm>({
     defaultValues: {
       email: "@naver.com",
     },
   });
-  const onValid = (data: any) => {
-    console.log(data);
+  const onValid = (data: IForm) => {
+  
+    if(data.password !== data.password1){
+      setError("password1", { message:"password are not the same"}, {shouldFocus:true});
+    }
+    //setError("extraError", {message: "Server offline"}); 
+
   };
   return (
     <div>
@@ -69,7 +48,11 @@ function ToDoList() {
         />
         <span>{errors?.email?.message}</span>
         <input
-          {...register("firstName", { required: "write here" })}
+          {...register("firstName", { 
+            required: "write here",
+            validate: (value) => 
+            value.includes("nico") ? "no nicos..":true,
+          })}
           placeholder="First Name"
         />
         <span>{errors?.firstName?.message}</span>
