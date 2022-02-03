49128a3c56cb54d846f4f989a824db287992f2f2
hwpphwpp <huiwonpark78@gmail.com>
2022-02-03 20:14:29 +0900

    2.React2_todo_2

--- 2.React_intermediate/todolist/src/ToDoList.tsx
+++ 2.React_intermediate/todolist/src/ToDoList.tsx
@@ -1,29 +1,108 @@
+import React from "react";
+import { useForm } from "react-hook-form";
 
-import {useForm} from "react-hook-form";
+/* function ToDoList() {
+  const [toDo, setToDo] = useState("");
+  const [toDoError, setToDoError] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setToDoError("");
+    setToDo(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (toDo.length < 10) {
+      return setToDoError("To do should be longer");
+    }
+    console.log("submit");
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} placeholder="Write a to do" />
+        <button>Add</button>
+        {toDoError !== "" ? toDoError : null}
+      </form>
+    </div>
+  );
+} */
 
-function ToDoList(){
-  const { register ,handleSubmit,formState } = useForm(); //handle~은 validation+precendDefault
-  const onValid = (data:any) =>{//데이터가 유효하지 않다면 useForm이 에러를 보여줌, 이 함수는 모든 유효성검사를 마쳤을 때만 호출된다
+interface IForm {
+  email: string;
+  firstName: string;
+  lastName: string;
+  username: string;
+  password: string;
+  password1: string;
+}
+
+function ToDoList() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IForm>({
+    defaultValues: {
+      email: "@naver.com",
+    },
+  });
+  const onValid = (data: any) => {
     console.log(data);
-  }; 
-  console.log(formState.errors);
-  return  (
-        <div>
-        <form 
-            style={{display:"flex", flexDirection: "column"}} 
-            onSubmit={handleSubmit(onValid)}>  {/* handle~이 받는 두 개의 인자-1.데이터가 유효할 때 호출되는 함수(필수) 2.~않을때~(필수X) */}
-          <input {...register("email", { required: true })} placeholder="Email" />
-          <input {...register("firstName", { required: true })} placeholder="First Name"/>
-          <input {...register("lastName", { required: true })}placeholder="Last Name"/>
-          <input {...register("username", { required: true, minLength: 10 })}placeholder="Username"/>
-          <input {...register("password", { required: true, minLength: 5 })} placeholder="Password"/>
-          <input {...register("password1", {required: "Password is required",minLength:{value: 5,
-                message: "Your password is too short.",},})}placeholder="Password1"/>
-        
-         <button>Add</button>
-        </form>
-        </div>
-      );
+  };
+  return (
+    <div>
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        <input
+          {...register("email", {
+            required: "Email is required",
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+              message: "Only naver.com emails allowed",
+            },
+          })}
+          placeholder="Email"
+        />
+        <span>{errors?.email?.message}</span>
+        <input
+          {...register("firstName", { required: "write here" })}
+          placeholder="First Name"
+        />
+        <span>{errors?.firstName?.message}</span>
+        <input
+          {...register("lastName", { required: "write here" })}
+          placeholder="Last Name"
+        />
+        <span>{errors?.lastName?.message}</span>
+        <input
+          {...register("username", { required: "write here", minLength: 10 })}
+          placeholder="Username"
+        />
+        <span>{errors?.username?.message}</span>
+        <input
+          {...register("password", { required: "write here", minLength: 5 })}
+          placeholder="Password"
+        />
+        <span>{errors?.password?.message}</span>
+        <input
+          {...register("password1", {
+            required: "Password is required",
+            minLength: {
+              value: 5,
+              message: "Your password is too short.",
+            },
+          })}
+          placeholder="Password1"
+        />
+        <span>{errors?.password1?.message}</span>
+        <button>Add</button>
+      </form>
+    </div>
+  );
 }
 
 export default ToDoList;
