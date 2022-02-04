eb75e885b434d0573ba45bb02f04b059968ec054
hwpphwpp <huiwonpark78@gmail.com>
2022-02-04 14:02:20 +0900

    2.React2_todo_6_refactoring

--- 2.React_intermediate/todolist/src/Components/ToDo.tsx
+++ 2.React_intermediate/todolist/src/Components/ToDo.tsx
@@ -1,14 +1,39 @@
-import { IToDo } from "../atoms";
+import React from "react";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atoms";
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+    
+    console.log("iwantto", event.currentTarget.name);
+
+};
   return (
     <li>
       <span>{text}</span>
-      <button>Doing</button>
-      <button>To Do</button>
-      <button>Done</button>
+      {category !== "DOING" && (
+        <button name="DOING" onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {category !== "TO_DO" && (
+        <button name="TO_DO" onClick={onClick}>
+          To Do
+        </button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={onClick}>
+          Done
+        </button>
+      )}
     </li>
   );
 }
 
-export default ToDo; 
+export default ToDo;
+
+
--- 2.React_intermediate/todolist/src/Components/ToDoList.tsx
+++ 2.React_intermediate/todolist/src/Components/ToDoList.tsx
@@ -5,14 +5,15 @@ import ToDo from "./ToDo";
 
 function ToDoList() {
   const toDos = useRecoilValue(toDoState);
+
   return (
     <div>
-      <h1>To Dos</h1>
-      <hr />
+      <h1>To Dos</h1><hr />
       <CreateToDo />
       <ul>
         {toDos.map((toDo) => (
-          <ToDo key={toDo.id} {...toDo} />//toDos배열의 toDo원소 하나하나가 ToDo 컴포넌트에 필요한 props와 같은 모양이기 때문
+          <ToDo key={toDo.id} {...toDo} />
+          //toDos배열의 toDo원소 하나하나가 ToDo 컴포넌트에 필요한 props와 같은 모양이기 때문
         ))}
       </ul>
     </div>
