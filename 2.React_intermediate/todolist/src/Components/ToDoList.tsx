e36a7e4e08888e9f1668379195bdf1961cbd42c7
hwpphwpp <huiwonpark78@gmail.com>
2022-02-06 21:11:12 +0900

    2.React2_todo_7_immutable3

--- 2.React_intermediate/todolist/src/Components/ToDoList.tsx
+++ 2.React_intermediate/todolist/src/Components/ToDoList.tsx
@@ -1,24 +1,42 @@
 import { useRecoilValue } from "recoil";
-import { toDoState } from "../atoms";
+import { toDoSelector, toDoState } from "../atoms";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
 function ToDoList() {
-  const toDos = useRecoilValue(toDoState);  //set하지않고 get만 하므로 useRecoilValue
-
+  const [toDo, doing, done]=useRecoilValue(toDoSelector);
+ 
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
       <CreateToDo /> 
-      {/* recoil, atom의 사용으로 어떤 prop도 전달할 필요가없다. */}
+
+      <h2>To Do</h2>
       <ul>
-        {toDos.map((toDo) => (
+        {toDo.map((toDo) => (
           <ToDo key={toDo.id} {...toDo} />
-          //toDos배열의 toDo원소 하나하나가 ToDo 컴포넌트에 필요한 props와 같은 모양이기 때문
-          //map함수 내에서 item을 render할 때 key가 필요 
         ))}
       </ul>
+      <hr />
+
+      
+      <h2>DOING</h2>
+      <ul>
+        {doing.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+      <hr />
+
+      <h2>DONE</h2>
+      <ul>
+        {done.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+      <hr />
+
     </div>
   );
 }
--- 2.React_intermediate/todolist/src/atoms.tsx
+++ 2.React_intermediate/todolist/src/atoms.tsx
@@ -1,4 +1,4 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export const toDoState=atom<IToDo[]>({
     key:"toDo",
@@ -12,3 +12,14 @@ export interface IToDo{
     category:"TO_DO"|"DOING"|"DONE";
   }
   
+export const toDoSelector = selector({
+  key:"toDoSelector",
+  get:({get})=>{//get function으로 atom을 받는다
+    const toDos=get(toDoState);
+    return [
+      toDos.filter((toDo)=>toDo.category==="TO_DO"),//카테고리TO_DO만 필터링하여 배열에 담기
+      toDos.filter((toDo)=>toDo.category==="DOING"),
+      toDos.filter((toDo)=>toDo.category==="DONE"),
+  ]  
+  }
+})
