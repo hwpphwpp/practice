d14278d48aef39353d5a67070ce99154b6d5a4f3
hwpphwpp <huiwonpark78@gmail.com>
2022-02-07 13:40:51 +0900

    2.React2_todo_8_selector

--- 2.React_intermediate/todolist/src/Components/ToDoList.tsx
+++ 2.React_intermediate/todolist/src/Components/ToDoList.tsx
@@ -1,25 +1,29 @@
 import React from "react";
-import { useRecoilValue } from "recoil";
-import { toDoSelector, toDoState } from "../atoms";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { categoryState, toDoSelector } from "../atoms";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
 function ToDoList() {
-  const [toDo, doing, done]=useRecoilValue(toDoSelector);
-  const onInput=(event:React.FormEvent<HTMLSelectElement>)=>{
-    console.log(event.currentTarget.value);
+  const toDos = useRecoilValue(toDoSelector); //한 개의 배열만 렌더
+  const [category, setCategory] = useRecoilState(categoryState);
+  const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+    setCategory(event.currentTarget.value);
   };
+  console.log(category);
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
-      <select onInput={onInput}>
-        <option value="TO_DO">To DO</option>
+      <select value={category} onInput={onInput}>
+        <option value="TO_DO">To Do</option>
         <option value="DOING">Doing</option>
         <option value="DONE">Done</option>
       </select>
-      <CreateToDo /> 
-
+      <CreateToDo />
+      {toDos?.map((toDo) => (
+        <ToDo key={toDo.id} {...toDo} />
+      ))}
     </div>
   );
 }
