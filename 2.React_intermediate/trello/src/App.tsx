ceb600e5e82f41558cd813891d4f75a1badc2ddd
hwpphwpp <huiwonpark78@gmail.com>
2022-02-09 16:22:37 +0900

    2.React_beautiful-dnd

---
+++ 2.React_intermediate/trello/src/App.tsx
@@ -0,0 +1,26 @@
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
+function App() {
+  const onDragEnd = () => {};
+  return (
+    <DragDropContext onDragEnd={onDragEnd}> 
+  {/* 유저가 드래그를 끝냈을 때 실행되는 필수 함수 onDragEnd */}
+      <div>
+        <Droppable droppableId="one">
+          {() => (
+            <ul>
+              <Draggable draggableId="first" index={0}>
+                {() => <li>One</li>}
+              </Draggable>
+              <Draggable draggableId="second" index={1}>
+                {() => <li>Two</li>}
+              </Draggable>
+            </ul>
+          )}
+        </Droppable>
+      </div>
+    </DragDropContext>
+  );
+}
+
+export default App; 
