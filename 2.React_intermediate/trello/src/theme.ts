2b6ab1e61bd828916bfa20e34cc5789c1efcf14e
hwpphwpp <huiwonpark78@gmail.com>
2022-02-11 21:50:07 +0900

    2.React_beautiful-dnd3

--- 2.React_intermediate/trello/src/theme.ts
+++ 2.React_intermediate/trello/src/theme.ts
@@ -2,7 +2,7 @@ import { DefaultTheme } from "styled-components";
 
 export const darkTheme: DefaultTheme = {
   bgColor: "#3F8Cf2",
-  boardColor:"#DADFE9",
   cardColor:"white",
+  boardColor:"#DADFE9",
 
 };
