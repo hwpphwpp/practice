1fe62f158e37d0e949877281f8586df8ba0110a4
hwpphwpp <huiwonpark78@gmail.com>
2022-02-12 19:41:15 +0900

    2.React_trello_reordering2

--- 2.React_intermediate/trello/src/App.tsx
+++ 2.React_intermediate/trello/src/App.tsx
@@ -38,8 +38,18 @@ const toDos = ["a", "b", "c", "d", "e", "f"];
 
 function App() {
   const [toDos, setToDos]=useRecoilState(toDoState); 
-  const onDragEnd = ({ destination, source}:DropResult) => {
-
+  const onDragEnd = ({ draggableId, destination, source}:DropResult) => {
+    //source: array에서 움직이고 싶은 item의 index, droppableId 제공
+    //새롭게 수정된 배열 return해줌
+    if(!destination) return; //이동하지 않고 제자리에 둘 경우 그대로 리턴 
+    setToDos(oldToDos=>{
+      const copyToDos = [...oldToDos]; //이전의 배열 copy (복사본 만들기)
+      //step1. source.index에서 아이템 삭제하기 
+      copyToDos.splice(source.index, 1)
+      //step2. destination.index로 item 돌려두기
+      copyToDos.splice(destination?.index, 0, draggableId) //draggableId=abcd..
+      return copyToDos;
+    })
 
   };
   return (
@@ -50,7 +60,8 @@ function App() {
             {(magic) => (
               <Board ref={magic.innerRef} {...magic.droppableProps}>
                 {toDos.map((toDo, index) => (
-                  <Draggable draggableId={toDo} index={index}>
+                  <Draggable key={toDo} draggableId={toDo} index={index}> 
+                  {/* key와 draggableId는 같아야한다 (beautiful dnd규칙) */}
                     {(magic) => (
                       <Card
                         ref={magic.innerRef}
