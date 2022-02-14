4e3faddd3a5ece8416bc7c200d97cd1d20860f16
hwpphwpp <huiwonpark78@gmail.com>
2022-02-14 12:56:59 +0900

    2.React_trello_styles

--- 2.React_intermediate/trello/src/Components/DragabbleCard.tsx
+++ 2.React_intermediate/trello/src/Components/DragabbleCard.tsx
@@ -1,35 +1,37 @@
 import React from "react";
-import {Draggable} from "react-beautiful-dnd";
+import { Draggable } from "react-beautiful-dnd";
 import styled from "styled-components";
 
-
-const Card = styled.div`
+const Card = styled.div<{ isDragging: boolean }>`
   border-radius: 5px;
   margin-bottom: 5px;
-  padding: 10px 10px;
-  background-color: ${(props) => props.theme.cardColor};
+  padding: 10px;
+  background-color: ${(props) =>
+    props.isDragging ? "#e4f2ff" : props.theme.cardColor};
+  box-shadow: ${(props) =>
+    props.isDragging ? "0px 2px 5px rgba(0, 0, 0, 0.5)" : "none"};
 `;
 
-interface IDragabbleCardProps{
-    toDo:string;
-    index:number
+interface IDragabbleCardProps {
+  toDo: string;
+  index: number;
 }
 
-function DragabbleCard({toDo, index}:IDragabbleCardProps){
-    return (
-        <Draggable key={toDo} draggableId={toDo} index={index}> 
-        {/* key와 draggableId는 같아야한다 (beautiful dnd규칙) */}
-          {(magic) => (
-            <Card
-              ref={magic.innerRef}
-              {...magic.dragHandleProps}
-              {...magic.draggableProps}
-            >
-              {toDo}
-            </Card>
-          )}
-        </Draggable>
-    );
+function DragabbleCard({ toDo, index }: IDragabbleCardProps) {
+  return (
+    <Draggable draggableId={toDo} index={index}>
+      {(magic, snapshot) => (
+        <Card
+          isDragging={snapshot.isDragging}
+          ref={magic.innerRef}
+          {...magic.dragHandleProps}
+          {...magic.draggableProps}
+        >
+          {toDo}
+        </Card>
+      )}
+    </Draggable>
+  );
 }
 
-export default React.memo(DragabbleCard); //prop이 같다면 리렌더링되는 것을 방지한다.
+export default React.memo(DragabbleCard);
