9709e8ab7ae60c319aa114f72af7919625d5d304
hwpphwpp <huiwonpark78@gmail.com>
2022-02-14 19:50:06 +0900

    2.React_animations_variants3

--- 2.React_intermediate/animations/src/App.tsx
+++ 2.React_intermediate/animations/src/App.tsx
@@ -12,19 +12,61 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
-  border-radius: 15px;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
+  display:grid;
+  grid-template-columns:repeat(2, 1fr);
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const myVars = { //초기, 최종 두 개의 stage(상태)를 가진다
- start: { scale:0 }, 
- end : {scale:1, rotateZ:360, transition:{type:"spring", delay:0.5} }
+const Circle = styled(motion.div)`
+  background-color:white;
+  height:70px;
+  width:70px;
+  border-radius:35px;
+  place-self:center;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+
+  `;
+
+  const boxVariants={
+    start: {
+      opacity:0,
+      scale:0.5
+    },
+    end: {
+      opacity:1,
+      scale:1,
+      transition:{
+        type:"spring",
+        duration:0.5,
+        bounce:0.5,
+        delayChildren:0.5,
+        staggerChildren:0.2, //framer motion의 기본 유틸리티로, 원마다 다른 딜레이 타임을 가지게 한다
+      },
+    },
+  }
+
+const circleVariants={
+  start:{
+    opacity:0,
+    y:10
+  },
+  end:{
+   opacity:1,
+   y:0,
+    },
 }
-function App() {
+
+function App() {  
   return (
     <Wrapper>
-      <Box variants={myVars} initial="start" animate="end"/>
+      <Box variants={boxVariants} initial="start" animate="end">
+        <Circle variants={circleVariants}/>
+        <Circle variants={circleVariants}/>
+        <Circle variants={circleVariants}/>
+        <Circle variants={circleVariants}/>
+      </Box>
     </Wrapper>
   );
 }
