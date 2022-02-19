d72f338b74fa4a045dddd9420b27ce05eb544b28
hwpphwpp <huiwonpark78@gmail.com>
2022-02-19 18:37:25 +0900

    2.React_netflix_slider

--- 2.React_intermediate/netclone/src/Routes/Home.tsx
+++ 2.React_intermediate/netclone/src/Routes/Home.tsx
@@ -1,56 +1,113 @@
 import { useQuery } from "react-query";
 import styled from "styled-components";
+import { motion, AnimatePresence } from "framer-motion";
 import { getMovies, IGetMoviesResult } from "../api";
-import {makeImagePath} from "../utils";
+import { makeImagePath } from "../utils";
+import { useState } from "react";
 
-const Wrapper =styled.div`
-  background:black;
-`;
-const Loader=styled.div`
-  height:20vh;
-  display:flex;
-  justify-content:center;
-  align-items:center;
+const Wrapper = styled.div`
+  background: black;
 `;
 
-const Banner=styled.div<{bgPhoto:string}>`
-  height:100vh; 
-  display:flex;
-  flex-direction:column;
-  justify-content:center;
-  padding:60px;
-
-  background-image:linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)),
-          url(${(props)=>props.bgPhoto});
-  background-size:cover;
+const Loader = styled.div`
+  height: 20vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
-const Title=styled.h2`
-  font-size:68px;
-  margin-bottom:20px;
+const Banner = styled.div<{ bgPhoto: string }>`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 60px;
+  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
+    url(${(props) => props.bgPhoto});
+  background-size: cover;
 `;
 
-const Overview=styled.p`
-  font-size:30px;
-  width:50%;
+const Title = styled.h2`
+  font-size: 68px;
+  margin-bottom: 20px; ;
 `;
+
+const Overview = styled.p`
+  font-size: 30px;
+  width: 50%;
+`;
+
+const Slider = styled.div`
+  position: relative;
+  top: -100px;
+`;
+
+const Row = styled(motion.div)`
+  display: grid;
+  gap: 10px;
+  grid-template-columns: repeat(6, 1fr);
+  position: absolute;
+  width: 100%;
+`;
+
+const Box = styled(motion.div)`
+  background-color: white;
+  height: 200px;
+  color: red;
+  font-size: 66px;
+`;
+
+const rowVariants = {
+  hidden: {
+    x: window.outerWidth + 10,
+  },
+  visible: {
+    x: 0,
+  },
+  exit: {
+    x: -window.outerWidth - 10,
+  },
+};
 
 function Home() {
-  const { data, isLoading } = useQuery<IGetMoviesResult>(["movies", "nowPlaying"], getMovies);
-  console.log(data, isLoading);
-  return (
-    <Wrapper>{isLoading ? (
-      <Loader>Loading..</Loader>
-     ) : (
-      // Fragment는 많은 요소를 공통된 부모없이 연이어 리턴할 수 있는 방법 
-       <>
-        <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
-          {/* Banner에서는 GET해온 영화의 배열에서 첫번째 항목을 보여줌 */}
-          <Title>{data?.results[0].title}</Title>
-          <Overview>{data?.results[0].overview}</Overview>
-        </Banner>
-       </>
-     )} </Wrapper>
+  const { data, isLoading } = useQuery<IGetMoviesResult>(
+    ["movies", "nowPlaying"],
+    getMovies
   );
-} 
+  const [index, setIndex] = useState(0);
+  const incraseIndex = () => setIndex((prev) => prev + 1);
+  return (
+    <Wrapper>
+      {isLoading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Banner
+            onClick={incraseIndex}
+            bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}
+          >
+            <Title>{data?.results[0].title}</Title>
+            <Overview>{data?.results[0].overview}</Overview>
+          </Banner>
+          <Slider>
+            <AnimatePresence>
+              <Row
+                variants={rowVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                transition={{ type: "tween", duration: 1 }}
+                key={index}
+              >
+                {[1, 2, 3, 4, 5, 6].map((i) => (
+                  <Box key={i}>{i}</Box>
+                ))}
+              </Row>
+            </AnimatePresence>
+          </Slider>
+        </>
+      )}
+    </Wrapper>
+  );
+}
 export default Home;
