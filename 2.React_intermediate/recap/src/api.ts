9768aff1a5c499beedfda402167cc9b52ce4632d
hwpphwpp <huiwonpark78@gmail.com>
2022-02-24 18:29:02 +0900

    ReactRecap_6_reactquery

--- 2.React_intermediate/recap/src/api.ts
+++ 2.React_intermediate/recap/src/api.ts
@@ -1,26 +1,17 @@
-const API_KEY = "10923b261ba94d897ac6b81148314a3f";
-const BASE_PATH = "https://api.themoviedb.org/3";
+const BASE_URL = `https://api.coinpaprika.com/v1`;
 
-interface IMovie{
-    id:number;
-    backdrop_path:string;
-    poster_path:string;
-    title:string;
-    overview:string;
+export function fetchCoins() {
+  return fetch(`${BASE_URL}/coins`).then((response) => response.json());
 }
 
-export interface IGetMoviesResult{
-    date:{
-        maximum:string;
-        minimum:string;
-    };
-    page:number;
-    results: IMovie[];
-    total_pages:number;
-    total_results:number;
+export function fetchCoinInfo(coinId: string) {
+  return fetch(`${BASE_URL}/coins/${coinId}`).then((response) =>
+    response.json()
+  );
 }
-export function getMovies() {
-  return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
-    (response) => response.json()
+
+export function fetchCoinTickers(coinId: string) {
+  return fetch(`${BASE_URL}/tickers/${coinId}`).then((response) =>
+    response.json()
   );
 }
--- 2.React_intermediate/recap/src/routes/Coins.tsx
+++ 2.React_intermediate/recap/src/routes/Coins.tsx
@@ -1,9 +1,13 @@
 import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { fetchCoins } from "../api";
 
 const Container = styled.div`
   padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
 `;
 
 const Header = styled.header`
@@ -21,9 +25,10 @@ const Coin = styled.li`
   border-radius: 15px;
   margin-bottom: 10px;
   a {
+    display: flex;
+    align-items: center;
     padding: 20px;
     transition: color 0.2s ease-in;
-    display: block;
   }
   &:hover {
     a {
@@ -41,13 +46,14 @@ const Loader = styled.span`
   text-align: center;
   display: block;
 `;
+
 const Img = styled.img`
   width: 35px;
   height: 35px;
   margin-right: 10px;
 `;
 
-interface CoinInterface {
+interface ICoin {
   id: string;
   name: string;
   symbol: string;
@@ -56,35 +62,31 @@ interface CoinInterface {
   is_active: boolean;
   type: string;
 }
- 
 
 function Coins() {
-  const [coins, setCoins] = useState<CoinInterface[]>([]);
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    (async () => {
-      const response = await fetch("https://api.coinpaprika.com/v1/coins");
-      const json = await response.json();
-      setCoins(json.slice(0, 100));
-      setLoading(false);
-    })();
-  }, []);
+  const { isLoading, data } = useQuery<ICoin[]>("allCoins", fetchCoins);
   return (
     <Container>
       <Header>
         <Title>코인</Title>
       </Header>
-      {loading ? (
+      {isLoading ? (
         <Loader>Loading...</Loader>
       ) : (
         <CoinsList>
-          {coins.map((coin) => (
+          {data?.slice(0, 100).map((coin) => (
             <Coin key={coin.id}>
-              <Link to={`/${coin.id}`}>
-              <Img
+              <Link
+                to={{
+                  pathname: `/${coin.id}`,
+                  state: { name: coin.name },
+                }}
+              >
+                <Img
                   src={`https://cryptoicon-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
                 />
-              {coin.name} &rarr;</Link>
+                {coin.name} &rarr;
+              </Link>
             </Coin>
           ))}
         </CoinsList>
