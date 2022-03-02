184000043b4d0db3da7d0ee4c75b0ebb8278e68b
hwpphwpp <huiwonpark78@gmail.com>
2022-03-02 17:13:46 +0900

    js-parcel-2

--- 3.Javascript/HackerNews/app.js
+++ 3.Javascript/HackerNews/app.js
@@ -1,40 +1,31 @@
-const container=document.getElementById('root');
-const content=document.createElement('div');
-
-const ajax=new XMLHttpRequest(); 
+const ajax=new XMLHttpRequest(); //elementbyid로 div를 셀렉했던 것처럼 출력 결과를 돌려준다. 
+                        //반환하는 값을 저장할 저장소가 필요함 -> let ajax 에 담음 (변수)
+                        //ajax를 통해 xmlhttprequest가 제공하는 도구들을 사용할 수 있게됨 
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
-const CONTENT_URL ='https://api.hnpwa.com/v0/item/@id.json';
+ajax.open('GET',NEWS_URL,false); //(method,url,async(false-동기적))
+//open은 데이터를 가져온 것이 아니고 실제로 데이터를 가져오는 것은 ajax가 제공하는 send 함수를 호출할 때 
+ajax.send(); 
 
-function getData(url){
-    ajax.open('GET',url,false); 
-    ajax.send();
-    return JSON.parse(ajax.response);
-}
+//데이터가 들어온 것을 확인해야함. 데이터는 response 값에 들어있음
+//console.log(ajax.response); //ui처리를 하기전에 브라우저에서 한번 확인
 
-const newsFeed=getData(NEWS_URL);  
+//출력 전에 중간 처리 과정이 필요, 가져온 데이터를 ul, li 태그로 
+//프리뷰 탭처럼 보기 편하게 그루핑되도록 처리. response값을 -> 객체로
 
-const ul = document.createElement('ul'); 
+const newsFeed = JSON.parse(ajax.response); //parse함수는 괄호 안에 입력으로 받은 json데이터를 객체로 바꿔서 반환해줌
+    //반환했으니 변수로 받아서 담아야함 (newsFeed). newsFeed에는 response값으로 온 JSON문자열이 객체화돼서 들어가있음 
 
-window.addEventListener('hashchange',function(){
-    const id=location.hash.substr(1);
-    const newsContent=getData(CONTENT_URL.replace('@id',id));
-    const title=document.createElement('h1');
-    title.innerHTML=newsContent.title; 
-    content.appendChild(title);    
-});
+//console.log(newsFeed);
 
+//직접 타이핑했던 ul li 태그를 코드로 작성하기 
+//문자열 만드는 방식 : 백틱 ( `` ) 사이에 만들고자하는 문자열을 넣어주면 됨 
+
+const ul = document.createElement('ul'); //document는 html을 조작하는데 필요한 모든 도구를 제공 
 for(let i=0; i<10; i++){ 
-    const div=document.createElement('div');
     const li=document.createElement('li');
-    const a = document.createElement('a');
-    div.innerHTML = `
-        <li>
-            <a href="#${newsFeed[i].id}">
-            ${newsFeed[i].title} (${newsFeed[i].comments_count})</a>
-        </li>
-        ` 
-    ul.appendChild(div.firstElementChild);
+    //매번 새로 만들어지니까 for문 안에서 만들어야함 
+    li.innerHTML=newsFeed[i].title;
+     ul.appendChild(li);
 }
 
-container.appendChild(ul);
-container.appendChild(content);
+document.getElementById('root').appendChild(ul);
--- 3.Javascript/HackerNews/dist/app.c328ef1a.js
+++ 3.Javascript/HackerNews/dist/app.c328ef1a.js
@@ -118,38 +118,35 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"app.js":[function(require,module,exports) {
-var container = document.getElementById('root');
-var content = document.createElement('div');
-var ajax = new XMLHttpRequest();
+var ajax = new XMLHttpRequest(); //elementbyid로 div를 셀렉했던 것처럼 출력 결과를 돌려준다. 
+//반환하는 값을 저장할 저장소가 필요함 -> let ajax 에 담음 (변수)
+//ajax를 통해 xmlhttprequest가 제공하는 도구들을 사용할 수 있게됨 
+
 var NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
-var CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
+ajax.open('GET', NEWS_URL, false); //(method,url,async(false-동기적))
+//open은 데이터를 가져온 것이 아니고 실제로 데이터를 가져오는 것은 ajax가 제공하는 send 함수를 호출할 때 
 
-function getData(url) {
-  ajax.open('GET', url, false);
-  ajax.send();
-  return JSON.parse(ajax.response);
-}
+ajax.send(); //데이터가 들어온 것을 확인해야함. 데이터는 response 값에 들어있음
+//console.log(ajax.response); //ui처리를 하기전에 브라우저에서 한번 확인
+//출력 전에 중간 처리 과정이 필요, 가져온 데이터를 ul, li 태그로 
+//프리뷰 탭처럼 보기 편하게 그루핑되도록 처리. response값을 -> 객체로
 
-var newsFeed = getData(NEWS_URL);
-var ul = document.createElement('ul');
-window.addEventListener('hashchange', function () {
-  var id = location.hash.substr(1);
-  var newsContent = getData(CONTENT_URL.replace('@id', id));
-  var title = document.createElement('h1');
-  title.innerHTML = newsContent.title;
-  content.appendChild(title);
-});
+var newsFeed = JSON.parse(ajax.response); //parse함수는 괄호 안에 입력으로 받은 json데이터를 객체로 바꿔서 반환해줌
+//반환했으니 변수로 받아서 담아야함 (newsFeed). newsFeed에는 response값으로 온 JSON문자열이 객체화돼서 들어가있음 
+//console.log(newsFeed);
+//직접 타이핑했던 ul li 태그를 코드로 작성하기 
+//문자열 만드는 방식 : 백틱 ( `` ) 사이에 만들고자하는 문자열을 넣어주면 됨 
+
+var ul = document.createElement('ul'); //document는 html을 조작하는데 필요한 모든 도구를 제공 
 
 for (var i = 0; i < 10; i++) {
-  var div = document.createElement('div');
-  var li = document.createElement('li');
-  var a = document.createElement('a');
-  div.innerHTML = "\n        <li>\n            <a href=\"#".concat(newsFeed[i].id, "\">\n            ").concat(newsFeed[i].title, " (").concat(newsFeed[i].comments_count, ")</a>\n        </li>\n        ");
-  ul.appendChild(div.firstElementChild);
+  var li = document.createElement('li'); //매번 새로 만들어지니까 for문 안에서 만들어야함 
+
+  li.innerHTML = newsFeed[i].title;
+  ul.appendChild(li);
 }
 
-container.appendChild(ul);
-container.appendChild(content);
+document.getElementById('root').appendChild(ul);
 },{}],"../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -178,7 +175,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56792" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56781" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
